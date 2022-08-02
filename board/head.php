3c9e97e1451bb08bdd0570e1ba3039bfcc5da79c
powerkyungil <92499273+powerkyungil@users.noreply.github.com>
2022-08-02 13:53:06 +0900

    Delete board directory

--- board/head.php
+++
@@ -1,7 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport"
-      content="width=device-width, initial-scale=1">
-<title>PHP 게시판 웹 사이트</title>
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
